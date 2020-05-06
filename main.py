1b624bc4d94961ce01c0abe90ef9aada8331438d
Daniel Jodłoś <danjod40@gmail.com>
2020-05-06 13:02:35 +0200

    Build db structure

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 import datetime
 from pandas import read_csv
+from mongoengine import *
 
-class Card:
+connect("flashcards")
 
-    question = None
-    answer = None
-    lastSeen = None
-    seenCounter = 0
-    seeIn = None
 
-    def __init__(self, q, a):
-        self.question = q
-        self.answer = a
+class User(Document):
+    username = StringField(max_length=20)
+    email = EmailField(max_length=40)
 
-    def see(self):
-        self.seenCounter += 1
-        return self.question, self.answer
+    def __str__(self):
+        return "%s (%s)" % (self.username, self.email)
 
-    def set_answer(self, answer):
-        self.seenCounter += 1
-        self.lastSeen = datetime.datetime.now()
-        self.seeIn = datetime.datetime.now() + datetime.timedelta(seconds=10*60)
+    def get_stats(self):
+        return UserStats.objects(user=self)[0]
+
+
+class Card(EmbeddedDocument):
+    question = StringField(max_length=100)
+    answer = StringField(max_length=200)
 
     def __str__(self):
         return "%s:\n----------------------------\n%s\n\n" % (self.question, self.answer)
 
 
-class Deck(object):
-    def __init__(self):
-        self.cards = []
+class Deck(Document):
+    cards = EmbeddedDocumentListField(Card)
+    author = ReferenceField(User)
+    name = StringField(max_length=100)
+    description = StringField(max_length=1000)
 
     def add_card(self, card):
         self.cards.append(card)
@@ -39,9 +39,48 @@ class Deck(object):
     def load_csv(self, filename):
         df = read_csv(filename, sep=';')
         for (q, a) in zip(df['question'], df['answer']):
-            self.add_card(Card(q, a))
+            self.add_card(Card(question=q, answer=a))
+        self.save()
 
     def to_csv(self, filename):
         with open(filename, "w+") as file:
             file.write("question;answer\n")
             file.writelines("%s;%s\n" % (a.question, a.answer) for a in self.cards)
+
+
+class CardStats(EmbeddedDocument):
+    card = EmbeddedDocumentField(Card)
+    history = ListField(IntField, default=[])
+    scheduled_for = DateTimeField(null=True)
+
+    def __str__(self):
+        return str(self.card)
+
+    def was_seen(self):
+        return len(self.history) > 0
+
+    def is_learnt(self):
+        return len(self.history) >= 2 and self.history[0] > 0 and self.history[1] > 0
+
+
+class DeckStats(EmbeddedDocument):
+    deck = LazyReferenceField(Deck)
+    deck_name = StringField()
+    stats = EmbeddedDocumentListField(CardStats)
+    unseen = EmbeddedDocumentListField(Card)
+
+    def get_first_n_unseen(self, n):
+        array = [CardStats(card=a) for a in self.unseen[0:(n - 1)]]
+        self.unseen = self.unseen[n:]
+        self.stats += array
+        return array
+
+
+class UserStats(Document):
+    user = ReferenceField(User)
+    deck_stats = EmbeddedDocumentListField(DeckStats)
+
+    def import_deck(self, deck):
+        s = DeckStats(deck=deck, deck_name=deck.name, stats=[], unseen=deck.cards)
+        self.deck_stats.append(s)
+        self.save()
